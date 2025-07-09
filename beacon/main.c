a0b46babea6b29d06dee867bb40913144f0e5d3f
Tijme Gommers <t.gommers@outlook.com>
2025-07-09 19:43:51 +0200

    Added support for C++.

--- beacon/main.c
+++ beacon/main.c
@@ -13,10 +13,19 @@
 #include <stdint.h>
 
 /**
- * The main function of your shellcode.
+ * The main function of your shellcode. In this example we solely return a value.
+ * 
+ * Using C++ instead:
+ *     If you want to use C++, rename this file so it has the `cpp` extension, then modify `makefile` to 
+ *     point `SOURCE_PATH` to the new filename. Also ensure `extern "C"` is prepended to the `EntryFunction`.
+ * 
+ * Popping `calc.exe`:
+ *     If you want a more extensive example of what Dittobytes can compile, instead of solely returning a 
+ *     number, the file below contains the full code to pop `calc.exe` on Windows AMD64.
+ *     https://github.com/tijme/dittobytes/blob/master/tests/win/amd64/1_functional_specific_api_call.c
  * 
  * @return uint64_t A return value of your shellcode.
  */
 uint64_t EntryFunction() {
-    return (uint64_t) 0x1234567812345678ULL;
+    return 0x1234567812345678ULL;
 }
